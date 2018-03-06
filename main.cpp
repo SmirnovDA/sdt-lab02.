c14c5ceca4e2ab3551568f59d059abbf9e0a9577
Данил Смирнов <danil2@me.com>
2018-03-06 10:34:46 +0300

    добавил сумму переменных

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 int main() {
     int a, b;
-    cout << "Enter A and B:"
-    cin >> a >> b;
+    cout << "Enter A and B:";
+    cin >> a >> b ;
+    cout << endl;
+    cout << "A + B = " << a + b << '\n';
+ system("pause");
 }
